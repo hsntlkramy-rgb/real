6cf88a3e60f5be840381176ee2d855a86c9d29c7
RealEstateFinder Developer <user@realestatefinder.com>
2025-08-19 23:31:17 +0300

    FIXED: Simplified main.tsx and added minimal test page to isolate white screen issue

--- RealEstateFinder/client/src/App.tsx
+++ RealEstateFinder/client/src/App.tsx
@@ -8,6 +8,7 @@ import PropertyDetailPage from './pages/property-detail';
 import TestUKPage from './pages/test-uk';
 import AuthPage from './pages/auth';
 import HomeSimplePage from './pages/home-simple';
+import TestMinimalPage from './pages/test-minimal';
 
 const queryClient = new QueryClient();
 
@@ -16,13 +17,14 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Switch>
-          {/* Use simple home page to avoid build errors */}
-          <Route path="/" component={HomeSimplePage} />
+          {/* Use minimal test page to isolate the issue */}
+          <Route path="/" component={TestMinimalPage} />
           <Route path="/map" component={MapPage} />
           <Route path="/swipe" component={SwipePage} />
           <Route path="/property/:id" component={PropertyDetailPage} />
           <Route path="/test-uk" component={TestUKPage} />
           <Route path="/auth" component={AuthPage} />
+          <Route path="/home" component={HomeSimplePage} />
         </Switch>
         <Toaster />
       </TooltipProvider>
--- RealEstateFinder/client/src/main.tsx
+++ RealEstateFinder/client/src/main.tsx
@@ -2,8 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// Force UK properties to be included in the build
-import { ukProperties } from "./data/properties-simple";
-console.log('🔧 Main entry point - UK Properties available:', ukProperties.length);
+// Simple main entry point without complex imports
+console.log('🔧 Main entry point - Starting RealEstateFinder app');
 
 createRoot(document.getElementById("root")!).render(<App />);
